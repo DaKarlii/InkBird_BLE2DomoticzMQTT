ad1cd51e29cf2116e72b4c6e435c03658731036a
DaKarlii <61060832+DaKarlii@users.noreply.github.com>
2021-04-26 14:07:50 +0200

    Add Domoticz Example

---
+++ src/domoticz.example.h
@@ -0,0 +1,3 @@
+#define DOMINTOPIC "domoticz/in"	// The Default Domoticz Topic
+#define DOMTEMPIDX 1      		// The First Domoticz Temp IDX (Create a Virtual Sensor for each Probe in ascending order in Domoticz)
+#define DOMBATTIDX 9      		// Create a Virtual Sensor with Percentage, for Batterie Status
